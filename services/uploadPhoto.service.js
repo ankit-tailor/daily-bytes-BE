f4eed7eef20731fd49a8935a206c55d4b91cbbe0
ankit-tailor <tailorankit3345@gmail.com>
2021-10-09 19:45:08 +0530

    fix: promise to open cluster connection

--- services/uploadPhoto.service.js
+++ services/uploadPhoto.service.js
@@ -1,14 +1,26 @@
 const multer = require('multer');
+const mongoose = require('mongoose');
+const Grid = require('gridfs-stream')
 const { GridFsStorage } = require('multer-gridfs-storage');
 require('dotenv').config();
 const mongoDbUrl = process.env.mongoDbUrl;
+
+const promise = mongoose.connect(mongoDbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const conn = mongoose.connection;
+let gfs;
+
+conn.once('open', () => {
+    gfs = Grid(conn, mongoose.mongo);
+    gfs.collection('uploads');
+});
 
 /**
  * Creates bucket of name "thumbnails" to at mongoDbUrl and upload file in that particular bucket
  */
 
 const storage = new GridFsStorage({
-    url: mongoDbUrl,
+    db: promise,
     options: { useNewUrlParser: true, useUnifiedTopology: true },
     file: (req, file) => {
         return {
