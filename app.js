60580d95690bac4c2087ce36ff3ee9d2ac4962b7
ankit-tailor <tailorankit3345@gmail.com>
2021-10-09 15:54:05 +0530

    project configure

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { connection } = require('./db')
+
+const news = require('./routes/news.routes');
+const PORT = 5000;
+
+// mongodb connection
+connection();
+
+app.use(cors())
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+    res.send("Hello worlddd")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on ${PORT}...`)
+});
+
+// Router
+app.use("/api/v1", news)
